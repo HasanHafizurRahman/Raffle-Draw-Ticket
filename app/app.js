b9b673acefd0694ca8da27e0a1ead5f2d09d9e80
Hasan Shanto <hasanshanto922@gmail.com>
2022-11-23 00:34:22 +0600

    error handling

--- app/app.js
+++ app/app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(
     [
         morgan('dev'),
-        cors()
+        cors(),
+        express.json()
     ]
 )
 
@@ -18,5 +19,18 @@ app.get('/health', (_req, res) => {
     res.status(200).json({message: "success"})
 })
 
+app.use((req, _res, next) => {
+    const error = new Error('Resource not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    if(error.status){
+        res.status(error.status).json({
+            message: error.message
+        })
+    }
+})
 
 module.exports = app;
