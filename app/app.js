691b1d62d5e5401fd39b15874633889206016ad3
Hasan Shanto <hasanshanto922@gmail.com>
2022-11-22 23:42:36 +0600

    middleware

--- app/app.js
+++ app/app.js
@@ -1,6 +1,18 @@
+require('dotenv').config('../.env')
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
 
 const app = express();
+
+// middleware
+app.use(
+    [
+        morgan('dev'),
+        cors()
+    ]
+)
 
 app.get('/health', (_req, res) => {
     res.status(200).json({message: "success"})
